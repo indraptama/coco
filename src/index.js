af56db7729be3c9dd0d096313e44e38a107f1cd0
Indra Pratama Putra <indra@indras-Mac-Pro.local>
2015-12-29 07:57:33 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('./css/style.css');
 require('./assets/img/cocosugar.jpg');
+require('./assets/img/mock1.jpg');
 import imgFill from './vendor/imgFill.js';
 
 imgFill('.imgFill', '.imgFillSrc');
