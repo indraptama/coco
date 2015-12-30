ad72df4e5c6642d6199753ff3dbfad6a27632467
Indra Pratama Putra <indra.symfonia@gmail.com>
2015-12-30 19:25:34 +0700

    update

---
+++ src/asset.js
@@ -0,0 +1,6 @@
+require('./css/style.css');
+require('./assets/img/cocosugar.jpg');
+require('./assets/img/mock1.jpg');
+require('./assets/img/mock2.jpg');
+require('./assets/img/mock3.jpg');
+require('./assets/img/mock4.jpg');
--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-require('./css/style.css');
-require('./assets/img/cocosugar.jpg');
-require('./assets/img/mock1.jpg');
-require('./assets/img/mock2.jpg');
-require('./assets/img/mock3.jpg');
-require('./assets/img/mock4.jpg');
+require('./asset.js');
+
 import imgFill from './vendor/imgFill.js';
 
 imgFill('.imgFill', '.imgFillSrc');
